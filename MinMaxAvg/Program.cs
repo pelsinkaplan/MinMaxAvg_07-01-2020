cab71a74ccc910b7ef17932a3eac86f184ad4245
pelsinkaplan <pelsinkaplan28@hotmail.com>
2021-01-07 21:10:14 +0300

    Project Created

--- MinMaxAvg/Program.cs
+++ MinMaxAvg/Program.cs
@@ -20,25 +20,25 @@ namespace MinMaxAvg
 
             sayılarList = sayılar.Split(' ').Select(Int32.Parse).ToList();
 
-            //for (int i = 0; i < sayılarList.Count; i++)
-            //{
-            //    if (sayılarList[i] > maxSayı)
-            //    {
-            //        maxSayı = sayılarList[i];
-            //    }
-            //    if (sayılarList[i] < minSayı)
-            //    {
-            //        minSayı = sayılarList[i];
-            //    }
-            //    ortalama += sayılarList[i];
-            //}
-            //ortalama /= sayılarList.Count;
-            //Console.WriteLine($"Max Sayı : {maxSayı} \nMin Sayı : {minSayı} \nOrtalama : {ortalama}");
-            //Console.ReadLine();
+            for (int i = 0; i < sayılarList.Count; i++)
+            {
+                if (sayılarList[i] > maxSayı)
+                {
+                    maxSayı = sayılarList[i];
+                }
+                if (sayılarList[i] < minSayı)
+                {
+                    minSayı = sayılarList[i];
+                }
+                ortalama += sayılarList[i];
+            }
+            ortalama /= sayılarList.Count;
+            Console.WriteLine($"Max Sayı : {maxSayı} \nMin Sayı : {minSayı} \nOrtalama : {ortalama}");
+            Console.ReadLine();
 
-            minSayı = sayılarList.Min();
-            maxSayı = sayılarList.Max();
-            ortalama = sayılarList.Average();
+            //minSayı = sayılarList.Min();
+            //maxSayı = sayılarList.Max();
+            //ortalama = sayılarList.Average();
 
             Console.WriteLine($"\nMax Sayı : {maxSayı} \nMin Sayı : {minSayı} \nOrtalama : {ortalama}");
             Console.ReadLine();
